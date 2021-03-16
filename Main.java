b8a69f1cda32c744eac5f9e36cc58dc3a280092e
LegendarySlowpoke <63976295+LegendarySlowpoke@users.noreply.github.com>
2021-03-16 12:40:29 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String args[]) {
-        new Worker().launch();
+        Worker worker = new Worker();
+        worker.launch();
     }
 }
--- StringValidator.java
+++ StringValidator.java
@@ -1,55 +1,55 @@
-public class StringValidator {
+    public class StringValidator {
 
-    private String strToCheck;
+        private String strToCheck;
 
-    protected StringValidator(String strToCheck) {
-        this.strToCheck = strToCheck;
-    }
-
-    protected boolean checkValidity() {
-        if (checkBracesAndOrder() && checkSymbols()) {
-            return true;
-        } else {
-            return false;
+        protected StringValidator(String strToCheck) {
+            this.strToCheck = strToCheck;
         }
-    }
 
-    private boolean checkBracesAndOrder() {
-        int braceChecker = 0;
-        for (int i = 0; i < strToCheck.length(); i++) {
-            if (String.valueOf(strToCheck.charAt(i)).equals("[")) {
-                braceChecker++;
-            } else if (String.valueOf(strToCheck.charAt(i)).equals("]")) {
-                braceChecker--;
+        protected boolean checkValidity() {
+            if (checkBracesAndOrder() && checkSymbols()) {
+                return true;
             } else {
-                if (String.valueOf(strToCheck.charAt(i)).matches("\\d") &&
-                        (i + 1) < strToCheck.length() - 1) {
-                    if (!String.valueOf(strToCheck.charAt(i + 1)).equals("[") &&
-                            !String.valueOf(strToCheck.charAt(i + 1)).matches("\\d")) {
-                        String str = String.valueOf(strToCheck.charAt(i + 1));
-                        return false;
-                    }
-                }
-            }
-            if (braceChecker < 0) {
                 return false;
             }
         }
-        if (braceChecker != 0) {
-            return false;
-        }
-        return true;
-    }
 
-    private boolean checkSymbols() {
-        strToCheck = strToCheck.replace('[', ' ');
-        strToCheck = strToCheck.replace(']', ' ');
-        strToCheck = strToCheck.replaceAll(" ", "");
-        strToCheck = strToCheck.replaceAll("[A-Za-z0-9]*", "");
-        if (strToCheck.equals("")) {
+        private boolean checkBracesAndOrder() {
+            int braceChecker = 0;
+            for (int i = 0; i < strToCheck.length(); i++) {
+                if (String.valueOf(strToCheck.charAt(i)).equals("[")) {
+                    braceChecker++;
+                } else if (String.valueOf(strToCheck.charAt(i)).equals("]")) {
+                    braceChecker--;
+                } else {
+                    if (String.valueOf(strToCheck.charAt(i)).matches("\\d") &&
+                            (i + 1) < strToCheck.length() - 1) {
+                        if (!String.valueOf(strToCheck.charAt(i + 1)).equals("[") &&
+                                !String.valueOf(strToCheck.charAt(i + 1)).matches("\\d")) {
+                            String str = String.valueOf(strToCheck.charAt(i + 1));
+                            return false;
+                        }
+                    }
+                }
+                if (braceChecker < 0) {
+                    return false;
+                }
+            }
+            if (braceChecker != 0) {
+                return false;
+            }
             return true;
-        } else {
-            return false;
+        }
+
+        private boolean checkSymbols() {
+            strToCheck = strToCheck.replace('[', ' ');
+            strToCheck = strToCheck.replace(']', ' ');
+            strToCheck = strToCheck.replaceAll(" ", "");
+            strToCheck = strToCheck.replaceAll("[A-Za-z0-9]*", "");
+            if (strToCheck.equals("")) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-}
